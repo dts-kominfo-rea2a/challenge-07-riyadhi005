3f978c1883b8a96aba7c6ff56d194ade30b11446
Riyadhi <faishal.riyadhi@gmail.com>
2022-06-12 20:49:27 +0800

    Challenge Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) =>{
+  let a = await promiseTheaterIXX();
+  let b = await promiseTheaterVGC();
+  let ab = a.concat(b);
+  let totalEmosi = 0;
+  ab.map((a) => a.hasil === emosi && (totalEmosi +=1));
+  return totalEmosi;
+};
 
 module.exports = {
   promiseOutput,
